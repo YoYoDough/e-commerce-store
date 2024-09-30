c79ef68e9d4a4a49adb42a1ad78a6447682072b6
YoYoDough <yosuf818s@gmail.com>
2024-09-29 21:07:25 -0700

    Every link in the footer section works.

---
+++ app/faq/page.jsx
@@ -0,0 +1,14 @@
+const page = () => {
+    return (
+        <div className = "w-full">
+            <div className = "footerContainer">
+                <h1 className = "footerHeader">Frequently Asked Questions</h1>
+                <div className = "footerContent">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed faucibus turpis in eu mi bibendum. Consectetur libero id faucibus nisl. Quisque id diam vel quam elementum. Eros donec ac odio tempor orci dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.</p>
+                    <p className = "text-sm italic mt-10">This document was last updated on September 290, 2024.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default page;
