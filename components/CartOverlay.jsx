bdaf307be35a4d498826e12575fef2022ee00ab6
YoYoDough <yosuf818s@gmail.com>
2024-10-02 22:06:56 -0700

    cart overlay works when you click on cart.

--- components/CartOverlay.jsx
+++ components/CartOverlay.jsx
@@ -1,22 +1,46 @@
-
+import {useState} from 'react'
 
 const CartOverlay = ({items, onClose}) => {
-  
+  const [selectedSize, setSelectedSize] = useState({});
+
 
   return (
     <div className = "cartOverlay">
         <button className="close-btn" onClick={onClose}>X</button>
-        <h2>Your Cart</h2>
-        <ul>
-            {items.map(item => (
-                <li>
-                    <img src = {item.imageUrl} alt = {item.name} className = "overlayImg"></img>
-                    <p>{item.name}</p>
-                    <p>Quantity: {item.count}</p>
-                    <p>${item.price * item.count}</p>
-                </li>
-            ))}
-        </ul>
+        <h2 className = "mb-5">Your Cart</h2>
+        <div >
+          <ul>
+              {items.map(item => (
+                  <li className = "cartItem">
+                      <img src = {item.imageUrl} alt = {item.name} className = "overlayImg"></img>
+                      <p>{item.name}</p>
+                      <p>Quantity: {item.count}</p>
+                      <p><b>${item.price * item.count}</b></p>
+                      {/* Size Selection Buttons */}
+                      <div className="size-selection">
+                        <button 
+                          onClick={() => handleSelectSize(item.id, 'small')}
+                          className={selectedSize[item.id] === 'small' ? 'selected' : ''}
+                        >
+                          Small
+                        </button>
+                        <button 
+                          onClick={() => handleSelectSize(item.id, 'medium')}
+                          className={selectedSize[item.id] === 'medium' ? 'selected' : ''}
+                        >
+                          Medium
+                        </button>
+                        <button 
+                          onClick={() => handleSelectSize(item.id, 'large')}
+                          className={selectedSize[item.id] === 'large' ? 'selected' : ''}
+                        >
+                          Large
+                        </button>
+                      </div>
+                  </li>
+              ))}
+          </ul>
+        </div>
     </div>
   )
 }
